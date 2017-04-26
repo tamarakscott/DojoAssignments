bdadc30092955bc4e1aaf9c974ec01061f447809
tamarakscott <tamarakscott@yahoo.com>
2017-04-25 22:12:18 -0500

    Add files via upload
    1st commit

---
+++ WebFundamentals/Javascript/random_chance.js
@@ -0,0 +1,11 @@
+function slotsgame(quarters){
+  var game=10;
+  var coin=1;
+Math.floor(Math.random() * 101);
+if (quarters==Math.random()) {
+  var winner=(Math.random()*101) + quarters;
+  console.log("You win; HOORAY" + winner)
+  else if (quarters!=Math.random) {
+    console.log("YOU LOSE")
+      }
+}
