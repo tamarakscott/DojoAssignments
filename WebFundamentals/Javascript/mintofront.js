15a71b6b333afadcab0dce9bbc103ce206fe9ef4
tamarakscott <tamarakscott@yahoo.com>
2017-04-25 22:13:19 -0500

    Add files via upload
    finished commit

---
+++ WebFundamentals/Javascript/mintofront.js
@@ -0,0 +1,13 @@
+var number=[4,2,1,3,5];
+var smallest=[0];
+for (i=0;i<number.length;i++){
+if(smallest<number[i]){
+smallest=number[i];
+number.push(smallest)
+
+
+    }
+      console.log(number);
+  
+}
+
